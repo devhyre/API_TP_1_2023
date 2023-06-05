cbc937061ae4b3c73cf0a814d08148f22c9e6e08
ctesenb <ctesenb@gmail.com>
2023-06-05 15:23:54 -0500

    MARCA POST PUT FIX

--- app/routes/private/brand.py
+++ app/routes/private/brand.py
@@ -8,19 +8,19 @@ from app.security.token import get_current_active_user
 
 brand_pr = APIRouter()
 
-@brand_pr.post('/crearMarca', response_model=Brand)
+@brand_pr.post('/crearMarca')
 async def crear_marca(brand: BrandPost, db: Session = Depends(get_db), user: ProfileResponse = Depends(get_current_active_user)):
     user_type = list(user.keys())[0]
     if user_type != 'admin':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='No tiene permisos para realizar esta acción')
     else:
-        new_brand = BrandModel(**brand.dict())
+        new_brand = BrandModel(name=brand.name, description=brand.description)
         db.add(new_brand)
         db.commit()
         db.refresh(new_brand)
         return new_brand
 
-@brand_pr.put('/actualizarMarca/{id}', response_model=Brand)
+@brand_pr.put('/actualizarMarca/{id}')
 async def actualizar_marca(id: int, brand: BrandPut, db: Session = Depends(get_db), user: ProfileResponse = Depends(get_current_active_user)):
     user_type = list(user.keys())[0]
     if user_type != 'admin':
@@ -30,6 +30,7 @@ async def actualizar_marca(id: int, brand: BrandPut, db: Session = Depends(get_d
         if not brand_db:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='No existe la marca')
         brand_db.name = brand.name
+        brand_db.description = brand.description
         db.commit()
         db.refresh(brand_db)
         return brand_db
