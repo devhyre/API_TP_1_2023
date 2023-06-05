01c610425da527354fbaa0eb50f9b5a0a8eaa040
ctesenb <ctesenb@gmail.com>
2023-06-05 18:31:41 -0500

    fix key target

--- app/models/detail_order_guide.py
+++ app/models/detail_order_guide.py
@@ -6,7 +6,7 @@ class DetailOrderGuide(Base):
     __tablename__ = "detail_order_guides"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sn_id = Column(String(255), ForeignKey("sns.id"))
+    sn_id = Column(String(255), ForeignKey("sns.sn_id"))
     order_guide_id = Column(Integer, ForeignKey("order_guides.id"))
 
     order_guide = relationship("OrderGuide", foreign_keys=[order_guide_id])
