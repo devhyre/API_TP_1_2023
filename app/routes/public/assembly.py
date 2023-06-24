abd5be495810310cb79d9b4e640bf7c283dcd21c
ctesenb <ctesenb@gmail.com>
2023-06-24 02:10:07 -0500

    asdasda jij jaja

--- app/routes/public/assembly.py
+++ app/routes/public/assembly.py
@@ -151,15 +151,32 @@ async def obtener_mejor_almacenamiento(placa_id: int, db: Session = Depends(get_
 #Mejor Cooler segun placa, procesador y case, si hubiera gpu tambien
 @assemblies_pu.get('/obtenerMejorCooler', status_code=status.HTTP_200_OK)
 async def obtener_mejor_cooler(placa_id: int, procesador_id: int, case_id: int, gpu_id: int = None, db: Session = Depends(get_db)):
-    #!OBTENER TODOS LOS PRODUCTOS
+    # Obtener todos los productos
     productos = db.query(ProductModel).all()
-    #!OBTENER TODOS LOS ASSEMBLIES RECOMENDADOS PARA LA PLACA
+
+    # Obtener todos los assemblies recomendados para la placa
     assemblies = db.query(AssemblyModel).filter(AssemblyModel.major_product_id == placa_id).all()
-    #!OBTENER TODOS LOS ASSEMBLIES RECOMENDADOS PARA EL PROCESADOR
+
+    # Obtener todos los assemblies recomendados para el procesador
     assemblies2 = db.query(AssemblyModel).filter(AssemblyModel.major_product_id == procesador_id).all()
-    #!OBTENER TODOS LOS ASSEMBLIES RECOMENDADOS PARA EL CASE
+
+    # Obtener todos los assemblies recomendados para el case
     assemblies3 = db.query(AssemblyModel).filter(AssemblyModel.major_product_id == case_id).all()
-    #!DATA A MOSTRAR
+
+    # Crear un conjunto para almacenar los IDs de productos repetidos
+    productos_repetidos = set()
+
+    if gpu_id is None:
+        for assembly in assemblies:
+            if assembly.product_id in {a.product_id for a in assemblies2} and assembly.product_id in {a.product_id for a in assemblies3}:
+                productos_repetidos.add(assembly.product_id)
+    else:
+        assemblies4 = db.query(AssemblyModel).filter(AssemblyModel.major_product_id == gpu_id).all()
+
+        for assembly in assemblies:
+            if assembly.product_id in {a.product_id for a in assemblies2} and assembly.product_id in {a.product_id for a in assemblies3} and assembly.product_id in {a.product_id for a in assemblies4}:
+                productos_repetidos.add(assembly.product_id)
+
     data = {
         "Id": [],
         "Name": [],
@@ -169,43 +186,17 @@ async def obtener_mejor_cooler(placa_id: int, procesador_id: int, case_id: int, 
         "Warranty": [],
         "Description": []
     }
-    #!BUSCAR QUE TODOS LOS PRODUCTOS SEAN DE LA CATEGORIA COOLER
-    #Si no hay gpu
-    if gpu_id == None:
-        for assembly in assemblies:
-            for assembly2 in assemblies2:
-                for assembly3 in assemblies3:
-                    if assembly.product_id == assembly2.product_id and assembly2.product_id == assembly3.product_id:
-                        for producto in productos:
-                            #CATEGORIA 15 O 16
-                            if assembly.product_id == producto.id and (producto.category_id == 15 or producto.category_id == 16):
-                                data["Id"].append(producto.id)
-                                data["Name"].append(producto.name)
-                                data["Price"].append(producto.price)
-                                data["Discount"].append(producto.discount)
-                                data["Ranking"].append(producto.ranking)
-                                data["Warranty"].append(producto.warranty)
-                                data["Description"].append(assembly.description)
-    #Si hay gpu
-    else:
-        #!OBTENER TODOS LOS ASSEMBLIES RECOMENDADOS PARA LA GPU
-        assemblies4 = db.query(AssemblyModel).filter(AssemblyModel.major_product_id == gpu_id).all()
 
-        for assembly in assemblies:
-            for assembly2 in assemblies2:
-                for assembly3 in assemblies3:
-                    for assembly4 in assemblies4:
-                        if assembly.product_id == assembly2.product_id and assembly2.product_id == assembly3.product_id and assembly3.product_id == assembly4.product_id:
-                            for producto in productos:
-                                if assembly.product_id == producto.id and (producto.category_id == 15 or producto.category_id == 16):
-                                    data["Id"].append(producto.id)
-                                    data["Name"].append(producto.name)
-                                    data["Price"].append(producto.price)
-                                    data["Discount"].append(producto.discount)
-                                    data["Ranking"].append(producto.ranking)
-                                    data["Warranty"].append(producto.warranty)
-                                    data["Description"].append(assembly.description)
-    #!DEVOLVER DATA
+    for producto in productos:
+        if producto.id in productos_repetidos and (producto.category_id == 15 or producto.category_id == 16):
+            data["Id"].append(producto.id)
+            data["Name"].append(producto.name)
+            data["Price"].append(producto.price)
+            data["Discount"].append(producto.discount)
+            data["Ranking"].append(producto.ranking)
+            data["Warranty"].append(producto.warranty)
+            data["Description"].append(assembly.description)
+
     return data
 
 #Mejor fuente segun placa, procesador y case, si hubiera gpu tambien
@@ -290,7 +281,7 @@ async def obtener_mejor_gpu(placa_id: int, procesador_id: int, case_id: int, db:
 
     # Verificar si un assembly está presente en las tres listas
     for assembly in assemblies:
-        if assembly.product_id in [a.product_id for a in assemblies2] and assembly.product_id in [a.product_id for a in assemblies3]:
+        if assembly.product_id in {a.product_id for a in assemblies2} and assembly.product_id in {a.product_id for a in assemblies3}:
             productos_repetidos.add(assembly.product_id)
 
     #!DATA A MOSTRAR
